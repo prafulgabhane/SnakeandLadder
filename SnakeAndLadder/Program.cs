b164bcd5ca153b7ac0192bb2767664e4ffd4829c
prafulgabhane <prafulgabhane786@gmail.com>
2022-09-21 15:36:10 +0530

    Adding to main

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC-1 Snake and Ladder game played with single player at start position 0
+            int StartPosition = 0;
+            Console.ReadLine();
+        }
+    }
+}
