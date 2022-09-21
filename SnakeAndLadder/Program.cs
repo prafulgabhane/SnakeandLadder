8d1671ce665756961d44999c2506dafe72afc2ca
prafulgabhane <prafulgabhane786@gmail.com>
2022-09-21 17:00:11 +0530

    Adding to UC3

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -10,14 +10,42 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            //UC-2 Player Rolls a die to get number between 1 to 6
+            //UC-3 Player then checks for a option there are no play ,ladder or snake
+            
+            int Start_Position = 0, ran = 0, choice = 0;
+            
+            //constant declaration
+            const int NOPLAY = 0;
+            const int LADDER = 1;
+            const int SNAKE = 2;
 
-            int Start_Position = 0; 
-            int ran = 0;
-            Random random = new Random(); //Random function can generate random number
-            ran = random.Next(1, 7);//Considering range from 1 to 6 i.e below 7
+            //Random function to generate random number
+            Random random = new Random();
+
+            //Considering range from 1 to 6 i.e below 7
+            ran = random.Next(1, 7); 
+
             Console.WriteLine("Rolled Dice");
-            Console.WriteLine("Number got: "+ ran);
+            Console.WriteLine("Number got: " + ran);
+
+            choice = random.Next(0, 3);
+            Console.WriteLine("Player Choice: " + choice);
+
+            switch (choice)
+            {
+                case NOPLAY:
+                    Console.WriteLine("Position: {0}", Start_Position);
+                    break;
+                case LADDER:
+                    Start_Position = Start_Position + ran;
+                    Console.WriteLine("Position: {0}", Start_Position);
+                    break;
+                case SNAKE:
+                    Start_Position = Start_Position - ran;
+                    Console.WriteLine("Position: {0}", Start_Position);
+                    break;
+            }
+
             Console.ReadLine();
         }
     }
